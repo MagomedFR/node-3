64411aeb2c902488a1cae9cfaccb16a7fd46138d
MagomedFR <magomed.khaptukaev@gmail.com>
2022-09-05 16:12:39 +0300

    Детская задача

---
+++ index.js
@@ -0,0 +1,6 @@
+var  figlet  =  require ( 'figlet' ) ;
+
+figlet ( 'Intocode' ,  function ( err ,  data )  { 
+    if  ( err )  { 
+        console.log ( ' Что - то пошло не так ... ' ) ; console.dir ( err ) ; return ; } console.log ( data ) } ) ;
+        
